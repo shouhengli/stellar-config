2ac26d7c9b45e6374781233c07bcf8274e663bd0
Chao Li <chao.li@data61.csiro.au>
2017-08-08 12:20:18 +1000

    Update redis connector with list operations

--- src/server/lib/config-store.js
+++ src/server/lib/config-store.js
@@ -7,40 +7,57 @@ P.promisifyAll(redis.Multi.prototype);
 
 const redisClient = redis.createClient();
 
-const errorHandler = (error) => {
+redisClient.on('error', (error) => {
   console.log(`Redis error caught by the global error handler: ${error}`);
-};
+});
 
-redisClient.on('error', errorHandler);
+function logRedisErrorAndThrow(error) {
+  console.log(`Redis error caught by the failure handler: ${error}`);
+  throw error;
+}
 
+function swallow() {}
+
+function setDef(namespace, name, content, defSetKey) {
+  const defKey = `${namespace}:${name}`;
+  return redisClient
+    .multi()
+    .set(defKey, content)
+    .sadd(defSetKey, defKey)
+    .execAsync()
+    .then(swallow, logRedisErrorAndThrow);
+}
+
+/**
+ * Represents a nonexistent key error in Redis.
+ */
 class NonexistentKeyError extends Error {
   constructor(key) {
     super(`Failed to delete nonexistent definition with key "${key}".`);
   }
 }
 
-const failureHandler = (error) => {
-  console.log(`Redis error caught by the failure handler: ${error}`);
-  throw error;
-};
+function delDef(namespace, name, defSetKey) {
+  const defKey = `${namespace}:${name}`;
 
-function setDef(prefix, name, content) {
   return redisClient
-    .setAsync(`${prefix}${name}`, content)
-    .catch(failureHandler);
-}
-
-function delDef(prefix, name) {
-  return redisClient
-    .del(`${prefix}${name}`)
+    .multi()
+    .srem(defSetKey, defKey)
+    .del(defKey)
     .then(
-      (deleted) => {
-        if (deleted === 0) {
-          throw new NonexistentKeyError(`${prefix}${name}`);
+      ([defSetMemberDeleted, defKeyDeleted]) => {
+        if (defSetMemberDeleted === 0 || defKeyDeleted === 0) {
+          throw new NonexistentKeyError(defKey);
         }
       },
-      failureHandler
+      logRedisErrorAndThrow
     );
+}
+
+function listDefNames(defSetKey) {
+  return redisClient
+    .smembersAsync(defSetKey)
+    .catch(logRedisErrorAndThrow);
 }
 
 /**
@@ -50,7 +67,7 @@ function delDef(prefix, name) {
  * @return {Promise} Will be resolved if the requested operation is successful.
  */
 function setSourceDef(name, content) {
-  return setDef(config.sourceKeyPrefix, name, content);
+  return setDef(config.namespaces.source, name, content, config.keys.sources);
 }
 
 /**
@@ -60,7 +77,16 @@ function setSourceDef(name, content) {
  *                   a {@link NonexistentKeyError} object.
  */
 function delSourceDef(name) {
-  return delDef(config.sourceKeyPrefix, name);
+  return delDef(config.namespaces.source, name, config.keys.sources);
+}
+
+/**
+ * Lists source definition names.
+ * @return {Promise<string[]>} Will be resolved with an array of definition names if the requested
+ *                             operation is successful.
+ */
+function listSourceDefNames() {
+  return listDefNames(config.keys.sources);
 }
 
 /**
@@ -70,7 +96,7 @@ function delSourceDef(name) {
  * @return {Promise} Will be resolved if the requested operation is successful.
  */
 function setMappingDef(name, content) {
-  return setDef(config.mappingKeyPrefix, name, content);
+  return setDef(config.namespaces.mapping, name, content, config.keys.mappings);
 }
 
 /**
@@ -80,7 +106,16 @@ function setMappingDef(name, content) {
  *                   a {@link NonexistentKeyError} object.
  */
 function delMappingDef(name) {
-  return delDef(config.mappingKeyPrefix, name);
+  return delDef(config.namespaces.mapping, name, config.keys.mappings);
+}
+
+/**
+ * Lists mapping definition names.
+ * @return {Promise<string[]>} Will be resolved with an array of definition names if the requested
+ *                             operation is successful.
+ */
+function listMappingDefNames() {
+  return listDefNames(config.keys.mappings);
 }
 
 /**
@@ -90,7 +125,7 @@ function delMappingDef(name) {
  * @return {Promise} Will be resolved if the requested operation is successful.
  */
 function setGraphSchemaDef(name, content) {
-  return setDef(config.graphSchemaKeyPrefix, name, content);
+  return setDef(config.namespaces.graphSchema, name, content, config.keys.graphSchemas);
 }
 
 /**
@@ -100,14 +135,26 @@ function setGraphSchemaDef(name, content) {
  *                   a {@link NonexistentKeyError} object.
  */
 function delGraphSchemaDef(name) {
-  return delDef(config.graphSchemaKeyPrefix, name);
+  return delDef(config.namespaces.graphSchema, name, config.keys.graphSchemas);
+}
+
+/**
+ * Lists graph schema definition names.
+ * @return {Promise<string[]>} Will be resolved with an array of definition names if the requested
+ *                             operation is successful.
+ */
+function listGraphSchemaDefNames() {
+  return listDefNames(config.keys.graphSchemas);
 }
 
 module.exports = {
   setSourceDef,
   delSourceDef,
+  listSourceDefNames,
   setMappingDef,
   delMappingDef,
+  listMappingDefNames,
   setGraphSchemaDef,
   delGraphSchemaDef,
+  listGraphSchemaDefNames,
 };
--- src/server/lib/util.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  doNothing() {
-    console.log('Doing nothing...');
-  },
-  doMore() {
-    /* Please don't... */
-    console.log('yeah i do...');
-  },
-};
